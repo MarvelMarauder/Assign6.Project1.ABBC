f9de78263410706cc3ee211545721982fff6442b
MarvelMarauder <90361775+MarvelMarauder@users.noreply.github.com>
2022-02-05 16:25:57 -0700

    Add project files.

---
+++ Assign6.Project1.ABBC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assign6.Project1.ABBC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
